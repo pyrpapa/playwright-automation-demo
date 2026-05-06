446ba650a3f3fcd9fda386838037bc730e6a5d47
pyrpa93 <pyrpa@proton.me>
2026-05-05 19:17:19 -0500

    Initial commit - smoke tests against the-internet

---
+++ UnitTest1.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.Playwright.NUnit;
+using NUnit.Framework;
+
+namespace PlaywrightAutomationDemo;
+
+[TestFixture]
+public class SmokeTests : PageTest
+{
+
+    public override BrowserNewContextOptions ContextOptions()
+{
+    return new BrowserNewContextOptions();
+}
+
+    public override async Task<IBrowser> NewBrowserAsync()
+    {
+        return await Playwright.Chromium.LaunchAsync(new BrowserTypeLaunchOptions
+        {
+            Headless = false,
+            SlowMo = 500
+        });
+    }
+    [Test]
+    public async Task HomePageLoads()
+    {
+        await Page.GotoAsync("https://the-internet.herokuapp.com");
+        await Expect(Page).ToHaveTitleAsync("The Internet");
+    }
+
+    [Test]
+    public async Task LoginPageVisible()
+    {
+        await Page.GotoAsync("https://the-internet.herokuapp.com/login");
+        await Expect(Page.Locator("h2")).ToContainTextAsync("Login Page");
+    }
+
+    [Test]
+    public async Task SuccessfulLogin()
+    {
+        await Page.GotoAsync("https://the-internet.herokuapp.com/login");
+        await Page.FillAsync("#username", "tomsmith");
+        await Page.FillAsync("#password", "SuperSecretPassword!");
+        await Page.ClickAsync("button[type='submit']");
+        await Expect(Page.Locator(".flash.success")).ToContainTextAsync("You logged into a secure area!");
+    }
+}
